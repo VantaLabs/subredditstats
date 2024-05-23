a93d363e6b4be1583a49d88ddf653e4e729e92a8
Corian (Cory) Kennedy <cory@darkintel.io>
2024-05-23 10:28:20 -0500

    Update srs.py

--- srs.py
+++ srs.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import requests
-import openai
 import os
+import openai
 from datetime import datetime
 
-# Load secrets
 OPENAI_API_KEY = st.secrets["openai_api_key"]
 
-# Initialize OpenAI client
-openai.api_key = OPENAI_API_KEY
 client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
 def fetch_subreddit_comments(subreddit, since, until, size=1000):
@@ -65,16 +62,13 @@ def analyze_comments_with_gpt4o(file_path):
         frequency_penalty=0,
         presence_penalty=0
     )
-    #return response['choices'][0]['message']['content']
-    st.write(response)
+    return response.choices[0].message.content
 
 def list_comment_files():
     return [file for file in os.listdir() if file.endswith("_comments.txt")]
 
-# Streamlit app
 st.title("Subreddit Comments Analysis")
 
-# Sidebar for historical analysis
 st.sidebar.title("Historical Analysis")
 comment_files = list_comment_files()
 selected_file = st.sidebar.selectbox("Select a comments file to review:", comment_files)
@@ -84,13 +78,11 @@ if selected_file:
         comments = file.read()
     st.sidebar.write(comments)
 
-# User inputs
 subreddit = st.text_input("Subreddit", "rabbitr1")
 start_date = st.date_input("Start Date")
 end_date = st.date_input("End Date")
 
 if st.button("Fetch and Analyze Comments"):
-    # Convert dates to timestamps
     since_timestamp = int(datetime.combine(start_date, datetime.min.time()).timestamp())
     until_timestamp = int(datetime.combine(end_date, datetime.min.time()).timestamp())
     
